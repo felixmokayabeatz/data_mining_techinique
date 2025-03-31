6258a4d10d3a74157f03bf3e01494d0a47882f34
felixmokayabeatz <130485404+felixmokayabeatz@users.noreply.github.com>
2025-03-31 14:07:09 +0300

    Add classify_view to handle insurance prediction requests

--- classify_project/classify_app/views.py
+++ classify_project/classify_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .classifier import predict_insurance
 
-# Create your views here.
+def classify_view(request):
+    result = None
+    if request.method == "POST":
+        age = int(request.POST["age"])
+        salary = int(request.POST["salary"])
+        result = predict_insurance(age, salary)
+    
+    return render(request, "classify.html", {"result": result})
